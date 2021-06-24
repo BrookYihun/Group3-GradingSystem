1022babfe17e2c32c7b1405d8fdd1383d7360c11
Binyam Kidane <bine5kid@gmail.com>
2021-06-24 13:54:21 +0300

    Implemneted calculator Interface

--- src/main/java/com/group3/gradecalculator/GradeCalculator.java
+++ src/main/java/com/group3/gradecalculator/GradeCalculator.java
@@ -9,6 +9,43 @@ package com.group3.gradecalculator;
  *
  * @author brook
  */
-public class GradeCalculator {
-    
+public class GradeCalculator implements GradeCalculatorInterface {
+
+    @Override
+    public char calculateGrade(float mark) {
+        
+        if (mark>=85 && mark<=100)
+            return 'A';
+        else if (mark>=70)
+            return 'B';
+        else if (mark>=55)
+            return 'C';
+        else if (mark>=45)
+            return 'D';
+        else if (mark>=0)
+            return 'F';
+        else
+            return 'Z';
+        
+    }
+
+    @Override
+    public float calculateTotal(float st, float sp, float rm, float cd, float cp) {
+        
+        st = st*5;
+        sp = sp*5;
+        cd = cd*5;
+        rm = rm*5;
+        cp = cp*7;
+        
+        return st + sp + cd + rm + cp;
+    }
+
+    @Override
+    public float calculateGpa(float total) {
+        
+        return (float) (total/27.0);
+        
+    }
+   
 }
